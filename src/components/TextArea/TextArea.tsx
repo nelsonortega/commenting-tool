7d9a93b88f5dc9e09e41c0b777e4404f69c67f98
Nelson <neor9712@gmail.com>
2021-08-15 10:01:15 -0600

    Empty comment validated
    Added an alert message when the comment is empty

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,7 +1,8 @@
 import './TextArea.css';
-import React, { ChangeEvent, useContext, useState } from 'react';
+import React, { ChangeEvent, useContext, useEffect, useState } from 'react';
 import CommentContext from '../../context/Comment/CommentContext';
 import CurrentUserContext from '../../context/CurrentUser/CurrentUserContext';
+import ShowComponent from '../ShowComponent/ShowComponent';
 
 interface ITextAreaProps {
   parentId?: string
@@ -9,22 +10,44 @@ interface ITextAreaProps {
 }
 
 function TextArea(props: ITextAreaProps) {
-  const [comment, setComment] = useState<string>('')
-  const { addComment, addReply } = useContext(CommentContext)
   const { currentUser } = useContext(CurrentUserContext)
+  const { addComment, addReply } = useContext(CommentContext)
+
+  const [comment, setComment] = useState<string>('')
+  const [showAlert, setShowAlert] = useState<boolean>(false)
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout
+
+    if (showAlert) {
+      timeout = setTimeout(() => {
+        setShowAlert(false)
+      }, 3000)
+    }
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [showAlert])
 
   const setTextAreaValue = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setComment(event.target.value)
   }
 
   const sendComment = () => {
+    if (!comment.trim()) {
+      setShowAlert(true)
+      setComment('')
+      return 
+    }
+
     if (props.parentId && props.closeReply) {
       addReply(currentUser, comment, props.parentId)
       props.closeReply()
-    } else {
+    } else 
       addComment(currentUser, comment)
-    }
 
+    setShowAlert(false)
     setComment('')
   }
 
@@ -37,6 +60,9 @@ function TextArea(props: ITextAreaProps) {
         placeholder="Write a comment!"
       />
       <div className="button-container">
+        <ShowComponent show={showAlert}> 
+          <label className="alert">Comment can't be empty!</label> 
+        </ShowComponent>
         <button 
           className="send-comment-button"
           onClick={sendComment}
