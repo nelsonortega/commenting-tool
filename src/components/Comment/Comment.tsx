d7f4516ae3d372f374dab40b0f7fb8612d7e02d7
Nelson <neor9712@gmail.com>
2021-08-15 19:53:54 -0600

    Show or Hide replies functionality
    Added the functionality for the user to be able to show or hide replies by clicking on the line on the left.

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -13,6 +13,7 @@ interface ICommentProps {
 }
 
 function Comment(props: ICommentProps) {
+  const [showReplies, setShowReplies] = useState<boolean>(true)
   const [showReplyTextArea, setShowReplyTextArea] = useState<boolean>(false)
 
   const { replies } = useContext(CommentContext)
@@ -25,32 +26,40 @@ function Comment(props: ICommentProps) {
     setShowReplyTextArea(prevState => !prevState)
   }
 
+  const showOrHideReplies = () => {
+    setShowReplies(prevState => !prevState)
+  }
+
   return (
     <React.Fragment>
       <Avatar user={props.user} avatarColor={props.avatarColor}/>
       <div className={'main-content-container'}>
-        <div className={'separator-line'}></div>
-        <div className={'comment-reply-container'}>
-          <div className={'comment-content'}>{props.content}</div>
-          <button className={'comment-reply-button'} onClick={showOrHideReplyTextArea}>Reply</button>
-          <ShowComponent show={showReplyTextArea}>
-            <TextArea 
-              parentId={props.id}
-              closeReply={showOrHideReplyTextArea}
-            />
-          </ShowComponent>
-          {commentReplies.map(comment => {
-            return (
-              <Comment 
-                id={comment.id}
-                key={comment.id}
-                user={comment.user} 
-                content={comment.content} 
-                avatarColor={comment.avatarColor}
-              />
-            )
-          })}
+        <div className={'separator-line-container'} onClick={showOrHideReplies}>
+          <div className={'separator-line'}></div>
         </div>
+        <ShowComponent show={showReplies}>
+          <div className={'comment-reply-container'}>
+            <div className={'comment-content'}>{props.content}</div>
+            <button className={'comment-reply-button'} onClick={showOrHideReplyTextArea}>Reply</button>
+            <ShowComponent show={showReplyTextArea}>
+              <TextArea 
+                parentId={props.id}
+                closeReply={showOrHideReplyTextArea}
+              />
+            </ShowComponent>
+            {commentReplies.map(comment => {
+              return (
+                <Comment 
+                  id={comment.id}
+                  key={comment.id}
+                  user={comment.user} 
+                  content={comment.content} 
+                  avatarColor={comment.avatarColor}
+                />
+              )
+            })}
+          </div>
+        </ShowComponent>
       </div>
     </React.Fragment>
   )
